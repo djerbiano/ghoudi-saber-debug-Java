afd5fa79926ff7d5794066d533b6dca43f296d01
Saber Ghoudi <saberghoudi2222@hotmail.fr>
2026-03-06 02:54:07 +0100

    Main implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,37 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Start symptom analysis...");
+		ISymptomReader reader = new ReadSymptomDataFromFile("resources/symptoms.txt");
+		ISymptomWriter writer = new WriteSymptomDataToFile("resources/result.out");
+
+		AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+		// Read symptoms
+		List<String> symptomsList = counter.getSymptoms();
+		System.out.println("Symptoms reading finished.");
+		System.out.println("Total symptoms read: " + symptomsList.size());
+
+		// Count symptoms
+		Map<String, Integer> symptomsCounter = counter.countSymptoms(symptomsList);
+		System.out.println("Symptoms counting finished.");
+		System.out.println("Different symptoms: " + symptomsCounter.size());
+
+		// Sort symptoms
+		Map<String, Integer> symptomsSorted = counter.sortSymptoms(symptomsCounter);
+		System.out.println("Symptoms sorting finished.");
+
+		// Write result in file
+		counter.writeSymptoms(symptomsSorted);
+		System.out.println("Results written to file: resources/result.out");
+
+		System.out.println("Analysis finished.");
+
+	}
+
+}
