e61caeec9fb390a1bd181f6e937e32abd061e123
Saber Ghoudi <saberghoudi2222@hotmail.fr>
2026-03-06 02:28:42 +0100

    Refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,47 +1,43 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+	ISymptomReader symptomReader;
+	ISymptomWriter symptomWriter;
+	Map<String, Integer> symptomCounts = new HashMap<>();
 
-	public static void main(String args[]) throws Exception {
-		Map<String, Integer> symptomCounts = new HashMap<>();
+	public AnalyticsCounter(ISymptomReader symptomReader, ISymptomWriter symptomWriter) {
+		this.symptomReader = symptomReader;
+		this.symptomWriter = symptomWriter;
+	}
 
-		try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
-			String line = reader.readLine();
+	public List<String> getSymptoms() {
+		return symptomReader.GetSymptoms();
+	}
 
-			while (line != null) {
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-					System.out.println("number of headaches: " + headacheCount);
-				} else if (line.equals("rash")) {
-					rashCount++;
-				} else if (line.contains("pupils")) {
-					pupilCount++;
-				}
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
 
-				line = reader.readLine();
+		for (String oneSymptom : symptoms) {
+			if (!symptomCounts.containsKey(oneSymptom)) {
+				symptomCounts.put(oneSymptom, 1);
+			} else {
+				symptomCounts.put(oneSymptom, symptomCounts.get(oneSymptom) + 1);
 			}
-		} catch (IOException e) {
-			System.out.println("Error reading file");
-
 		}
-
-		// Writes symptom data
-		symptomCounts.put("headache", headacheCount);
-		symptomCounts.put("rash", rashCount);
-		symptomCounts.put("dialated pupils", pupilCount);
-
-		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
-		writer.writeSymptoms(symptomCounts);
+		return symptomCounts;
 
 	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		return new TreeMap<>(symptoms);
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		symptomWriter.writeSymptoms(symptoms);
+	}
+
 }
